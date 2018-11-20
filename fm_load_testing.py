5941e105aff55914e4adeec5134a3a8b20b915e7
Defixer <jp.mendoza017@gmail.com>
2018-11-20 09:00:29 +0800

    Add unique user creation

--- fm_load_testing.py
+++ fm_load_testing.py
@@ -1,17 +1,22 @@
 from locust import HttpLocust, TaskSet, task
+from locust.events import request_failure
 import json
 import random
-from locust.events import request_failure
+import csv
+import logging
 
 client_secret = "/Users/crmonlinegraph/Documents/Scripts/fm_load_testing/client_secret.json"
-user_creds = "/Users/crmonlinegraph/Documents/Scripts/fm_load_testing/client_secret.json"
+user_creds = "/Users/crmonlinegraph/Documents/Scripts/fm_load_testing/user_creds.csv"
+USER_CREDENTIALS = None
 
-class UserBehavior(TaskSet):
+class FM_load_testing(TaskSet):
 	def on_start(self):
 		""" on_start is called when a Locust start before 
 			any task is scheduled
 		"""
 		self.set_environment_variables()
+		if len(USER_CREDENTIALS) > 0:
+			self.username, self.password = USER_CREDENTIALS.pop() #removes username/password from USER_CREDENTIALS list after being hatched
 
 
 	def set_environment_variables(self):
@@ -21,29 +26,27 @@ class UserBehavior(TaskSet):
 		self.user_id = data['user_id'] #get from Manage Users > click on User > get the alphanumeric code in URL bar; also available in Local Storage of Developer Tools
 		self.client_id = data['client_id'] #get from Developer Tools (F12) > Application tab > Storage > Local Storage > site instance > current_client json
 
-	# @task(1) #n in all @task(n) is the ratio of how each function will run in the given swarm	
-	# def access_homepage(self):
-	# 	headers = {'oauth-token': self.token}
-	# 	self.client.get("/", headers=headers)
-
-	# @task(1)
-	# def fetch_accounts(self):
-	# 	headers = {'oauth-token': self.token}
-	# 	response = self.client.get("/rest/v10/Accounts", headers=headers)
-
-	@task(1)
+	@task(1) #@task(n) where n is the ratio of how each function will run in the given swarm	
 	def fetch_leads(self):
-		login_creds = (self.auth_username, self.auth_password)
+		login_creds = (self.username, self.password)
 		headers = {'Content-Type': 'application/json',
 					'Client-Id': self.client_id}
 		full_result = "/leads"
 		filtered_result = "/leads?page=1&first_name=&last_name&company&date_follow_up&status&email_address"
-		response = self.client.get(full_result, auth=login_creds, headers=headers)
-		# print("{}".format(response.json()))
-	@task(1)
+		logging.info("Fetch Leads | Username: {}\tPassword: {}".format(self.username, self.password))
+		response = self.client.get(full_result, auth=login_creds, headers=headers)	
+	
 		
-class WebsiteUser(HttpLocust): #min_wait & max_wait (in milliseconds; default 1000 if not declared); simulated user to wait before executing each task
+class FM_User(HttpLocust): #min_wait & max_wait (in milliseconds; default 1000 if not declared); simulated user to wait before executing each task
 	host = 'https://uatapi.fieldmagic.co'
-	task_set = UserBehavior
-	min_wait = 5000
-	max_wait = 10000
+	task_set = FM_load_testing
+	min_wait = 5000 #minimum of n millisec before executing a task per user
+	max_wait = 10000 #maximum of n millisec before executing a task per user
+
+	def __init__(self): #fetch username/password from csv file
+		super(FM_User, self).__init__()
+		global USER_CREDENTIALS
+		if (USER_CREDENTIALS == None):
+			with open(user_creds, 'rb') as file:
+				reader = csv.reader(file)
+				USER_CREDENTIALS = list(reader)
